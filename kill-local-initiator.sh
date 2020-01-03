792258c5db24d6e189220dde6157315c2933e7eb
James Howes <james.howes@digital.cabinet-office.gov.uk>
2020-01-03 10:16:45 +0000

    Don't exit script if PID cannot be found
    - If the PID doesnb't exist then we assume the application has already stopped so carry
    on running the rest of the script and remove the pid file.

--- kill-local-initiator.sh
+++ kill-local-initiator.sh
@@ -4,6 +4,6 @@ set -e
 
   if [ -f "./tmp/pids/local-initiator.pid" ]; then
   echo "Killing local-initiator"
-  kill "$(< ./tmp/pids/local-initiator.pid)"
+  kill "$(< ./tmp/pids/local-initiator.pid)" || true
   rm ./tmp/pids/local-initiator.pid
   fi
--- kill-local-receiver.sh
+++ kill-local-receiver.sh
@@ -4,6 +4,6 @@ set -e
 
   if [ -f "./tmp/pids/local-receiver.pid" ]; then
   echo "Killing local-receiver"
-  kill "$(< ./tmp/pids/local-receiver.pid)"
+  kill "$(< ./tmp/pids/local-receiver.pid)" || true
   rm ./tmp/pids/local-receiver.pid
   fi
